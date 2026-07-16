40c38d6820c7efc40efd91d19b169111bbd124f3
t0dysseus <mdxppbzwdt@privaterelay.appleid.com>
2026-07-16 22:36:00 +0200

    Add files via upload

--- presentation-data.js
+++ presentation-data.js
@@ -597,7 +597,7 @@ detail4x2: {
       // Spalte 2: der Teil mit den Pfeilen
       '<div style="background:#fff; border-radius:12px; padding:24px; box-shadow:0 2px 12px rgba(0,0,0,0.06); border-left:5px solid #e76f51; height:100%; box-sizing:border-box;">' +
         '<h3 style="margin:0 0 12px 0; font-size:1.1em; font-weight:700; color:#111;">Kausalkette</h3>' +
-        '<p style="margin:0; color:#444; line-height:1.5; font-size:0.95em;">--> mehr Eigenverantwortung<br>--&gt; Selbstvertrauen<br>--&gt; mehr Motivation<br>--&gt; Selbstbestimmung</p>' +
+        '<p style="margin:0; color:#444; line-height:1.5; font-size:0.95em;">--> mehr Eigenverantwortung<br>--&gt; Selbstvertrauen<br>--&gt; mehr Motivation<br> </p>' +
       '</div>',
       // Spalte 3: Validierung
       '<div style="background:#fff; border-radius:12px; padding:24px; box-shadow:0 2px 12px rgba(0,0,0,0.06); border-left:5px solid #2a9d8f; height:100%; box-sizing:border-box;">' +
